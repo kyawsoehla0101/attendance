699199a408930eb1c5ce57919fb94e93076a861e
unknown <kyawsoehla@gmail.com>
2025-06-30 12:57:11 +0630

    Add .gitignore file for Django project

--- qrattendance/settings.py
+++ qrattendance/settings.py
@@ -82,13 +82,22 @@ WSGI_APPLICATION = 'qrattendance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'attendance',  # your database name
+        'USER': 'root',  # your MySQL username
+        'PASSWORD': 'kyawsoehla0101',  # your MySQL password
+        'HOST': 'localhost',  # or use IP address if needed
+        'PORT': '3306',  # default MySQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
